43213503d9fb14859a9a253b2927bf690a55326e
ElrohirGT <elrohirgt@gmail.com>
2025-07-07 20:23:47 -0600

    faet: Added env

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Compiler Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      python = pkgs.python3.withPackages (p: [p.antlr4-python3-runtime]);
+    in {
+      default = pkgs.mkShell {
+        packages = [pkgs.antlr4 python pkgs.entr];
+      };
+    });
+  };
+}
